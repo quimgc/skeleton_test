1a40177ec5ac0969b10c9bb28637c1d2ebeb467d
quimgc <quimgonzalez@iesebre.com>
2017-10-06 17:35:14 +0200

    sync

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+     factory(Task::class, 500)->create();
     }
 }
